b8bee0403736fd1dd35ed5a0423fe28360457e7c
simon699 <shifengwei88@126.com>
2024-06-28 11:31:58 +0800

    提交

--- subtitleV3/settings.py
+++ subtitleV3/settings.py
@@ -103,7 +103,7 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.getenv('PROD_DB_NAME'),
             'USER': os.getenv('PROD_DB_USER'),
             'PASSWORD': os.getenv('PROD_DB_PASSWORD'),
